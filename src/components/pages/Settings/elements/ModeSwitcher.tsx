50fc5e273957d657a8c2185d522cba3e39608a74
Milosz <miloszbaranski@outlook.com>
2022-08-25 22:53:21 +0200

    import renamed enum

--- src/components/pages/Settings/elements/ModeSwitcher.tsx
+++ src/components/pages/Settings/elements/ModeSwitcher.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { ESettingsKeys } from "enums/SettingsKeys";
+import { ESettingsKeys } from "enums/ESettingsKeys";
 
 interface Props {
   isLightMode: boolean;
