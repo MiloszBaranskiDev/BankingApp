19074bd81c67cfbd770e692a33eff85c094be5ef
Milosz <miloszbaranski@outlook.com>
2022-09-08 23:51:25 +0200

    use enum

--- src/redux/slices/TransactionsSlice.ts
+++ src/redux/slices/TransactionsSlice.ts
@@ -1,9 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import { ECurrenciesSymbols } from "enums/ECurrenciesSymbols";
 import { ITransaction } from "interfaces/ITransaction";
+import { TransactionCategory } from "enums/TransactionCategory";
 
 const initialState: ITransaction[] = [
   {
-    category: "Outgoing transfer",
+    category: TransactionCategory.outgoing,
     date: "2022-06-11",
     id: "6r6deb4d-6b6d-4bad-9bdd-1b0d7b3dcb6d",
     details: [
@@ -21,7 +24,7 @@ const initialState: ITransaction[] = [
       },
       {
         label: "Currency",
-        value: "PLN",
+        value: ECurrenciesSymbols.pln,
       },
       {
         label: "Amount",
@@ -34,17 +37,48 @@ const initialState: ITransaction[] = [
     ],
   },
   {
-    category: "Currency swap",
+    category: TransactionCategory.incoming,
+    date: "2022-06-12",
+    id: "2r6aeb1d-6b6d-4bad-9bdd-1b0d7b3dcb6d",
+    details: [
+      {
+        label: "Sender",
+        value: "John Bloggs",
+      },
+      {
+        label: "Account No.",
+        value: "123456789987654321",
+      },
+      {
+        label: "Address",
+        value: "Lorem ipsum 16/4",
+      },
+      {
+        label: "Currency",
+        value: ECurrenciesSymbols.pln,
+      },
+      {
+        label: "Amount",
+        value: 1250,
+      },
+      {
+        label: "Title",
+        value: "Rent",
+      },
+    ],
+  },
+  {
+    category: TransactionCategory.swap,
     date: "2022-06-13",
     id: "3r3deb3d-6b6d-4bad-9bdd-1b0d7b3dcb6d",
     details: [
       {
         label: "From",
-        value: "88 USD",
+        value: `88 ${ECurrenciesSymbols.usd.toUpperCase()}`,
       },
       {
         label: "To",
-        value: "396 PLN",
+        value: `396 ${ECurrenciesSymbols.pln.toUpperCase()}`,
       },
       {
         label: "Exchange rate",
