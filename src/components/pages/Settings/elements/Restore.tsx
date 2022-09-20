b784e9d03efa3ae8f701afa2982f71a61ba7922a
Milosz <miloszbaranski@outlook.com>
2022-09-21 00:08:38 +0200

    add margins

--- src/components/pages/Settings/elements/Restore.tsx
+++ src/components/pages/Settings/elements/Restore.tsx
@@ -1,6 +1,7 @@
 import { Dispatch } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { restoreApp } from "redux/slices/SettingsSlice";
+import styled from "styled-components";
 
 import StyledButton from "components/styled/StyledButton";
 
@@ -8,13 +9,20 @@ const Restore: React.FC = () => {
   const dispatch: Dispatch = useDispatch();
 
   return (
-    <div>
+    <StyledRestore>
       <p>Restore app to default data and settings</p>
       <StyledButton onClick={() => dispatch(restoreApp())}>
         Restore
       </StyledButton>
-    </div>
+    </StyledRestore>
   );
 };
 
 export default Restore;
+
+const StyledRestore = styled.div`
+  margin-top: 20px;
+  p {
+    margin-bottom: 10px;
+  }
+`;
