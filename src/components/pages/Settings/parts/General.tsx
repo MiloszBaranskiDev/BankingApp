fd664f353675d4d85b33305243e25e87e6a4674f
Milosz <miloszbaranski@outlook.com>
2022-09-21 00:07:11 +0200

    rename

---
+++ src/components/pages/Settings/parts/General.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+import { ESettingsKeys } from "enums/ESettingsKeys";
+import { ISettings } from "interfaces/ISettings";
+
+import StyledHeading from "components/styled/StyledHeading";
+import FavouriteCurrency from "../elements/FavouriteCurrency";
+import Restore from "../elements/Restore";
+
+interface Props {
+  settings: ISettings;
+  handleSettingsChange: (key: ESettingsKeys, value: string | boolean) => void;
+}
+
+const General: React.FC<Props> = ({ settings, handleSettingsChange }) => {
+  return (
+    <StyledGeneral>
+      <StyledHeading>General</StyledHeading>
+      <FavouriteCurrency
+        favouriteCurrency={settings[ESettingsKeys.favouriteCurrency]}
+        handleSettingsChange={handleSettingsChange}
+      />
+      <Restore />
+    </StyledGeneral>
+  );
+};
+
+export default General;
+
+const StyledGeneral = styled.div`
+  padding: ${(props) => props.theme.tilePadding};
+  border-radius: ${(props) => props.theme.radius};
+  background-color: ${(props) => props.theme.colors.bgc};
+  box-shadow: ${(props) => props.theme.shadow};
+  @media (min-width: ${(props) => props.theme.breakpoints.tablet}) {
+    flex-basis: 49.3%;
+  }
+`;
