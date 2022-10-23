cab8f859f94c9ddfa71a4d5567ce954516f5984c
Milosz <miloszbaranski@outlook.com>
2022-10-24 01:32:37 +0200

    edit enum

--- src/interfaces/ITransaction.ts
+++ src/interfaces/ITransaction.ts
@@ -1,11 +1,6 @@
-interface IDetail {
-  label: string;
-  value: string | number | boolean;
-}
-
 export interface ITransaction {
   category: string;
   id: string;
   date: string;
-  details: IDetail[];
+  details: object;
 }
