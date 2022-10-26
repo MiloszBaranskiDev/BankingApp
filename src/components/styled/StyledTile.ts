ff9e56b4b4079b373dd8e287edf9a5a2e87bf8d6
Milosz <miloszbaranski@outlook.com>
2022-10-27 00:56:23 +0200

    add reusable tile component

---
+++ src/components/styled/StyledTile.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledTile = styled.div`
+  background-color: ${(props) => props.theme.colors.bgc};
+  padding: ${(props) => props.theme.tilePadding};
+  border-radius: ${(props) => props.theme.radius};
+  box-shadow: ${(props) => props.theme.shadow};
+`;
+
+export default StyledTile;
