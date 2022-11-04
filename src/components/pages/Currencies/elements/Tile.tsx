835e0c8975d81430d381e71106db7b22d952f41e
Milosz <miloszbaranski@outlook.com>
2022-11-04 01:07:00 +0100

    fix spacing

--- src/components/pages/Currencies/elements/Tile.tsx
+++ src/components/pages/Currencies/elements/Tile.tsx
@@ -21,17 +21,17 @@ const StyledTile = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-bottom: 24px;
+  margin-bottom: 20px;
   background-color: ${(props) => props.theme.colors.bgc};
   padding: ${(props) => props.theme.tilePadding};
   border-radius: ${(props) => props.theme.radius};
   box-shadow: ${(props) => props.theme.shadow};
   @media (min-width: ${(props) => props.theme.breakpoints.tablet}) {
-    flex-basis: 49.3%;
-    margin-bottom: 0;
+    flex-basis: 49%;
   }
   @media (min-width: ${(props) => props.theme.breakpoints.desktop}) {
     flex-basis: 23.8%;
+    margin-bottom: 0;
   }
 `;
 
