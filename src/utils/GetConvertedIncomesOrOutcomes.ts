74c3115b3affdf1ada6f2fbd400dce146e12040d
Milosz <miloszbaranski@outlook.com>
2022-11-09 01:39:09 +0100

    calculate outcomes or incomes and convert them to PLN

---
+++ src/utils/GetConvertedIncomesOrOutcomes.ts
@@ -0,0 +1,29 @@
+import { ETransationType } from "enums/ETransactionType";
+import { ICurrencyRate } from "interfaces/ICurrencyRate";
+import { ITransaction } from "interfaces/ITransaction";
+import { ECurrencySymbol } from "enums/ECurrencySymbol";
+
+const GetConvertedIncomesOrOutcomes = (
+  transactionType: Exclude<ETransationType, ETransationType.swap>,
+  transactions: ITransaction[],
+  currenciesRates: ICurrencyRate[]
+): number => {
+  let result: number = 0;
+
+  transactions
+    .filter((transaction) => transaction.type === transactionType)
+    .map((transaction) => {
+      return (result +=
+        transaction.currencySymbol === ECurrencySymbol.pln
+          ? transaction.amount! * 1
+          : transaction.amount! *
+            currenciesRates.find(
+              (currencyRate) =>
+                currencyRate.symbol === transaction.currencySymbol
+            )?.price!);
+    });
+
+  return result;
+};
+
+export default GetConvertedIncomesOrOutcomes;
