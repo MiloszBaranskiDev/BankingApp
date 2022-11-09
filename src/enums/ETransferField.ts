d2344c16a95c254d4c00d0964f91ddf6431dac61
Milosz <miloszbaranski@outlook.com>
2022-11-09 01:38:04 +0100

    renamed

--- src/enums/ETransferField.ts
+++
@@ -1,9 +0,0 @@
-export enum ETransferField {
-  title = "title",
-  recipient = "recipient",
-  sender = "sender",
-  account = "account",
-  address = "address",
-  currencySymbol = "currency_symbol",
-  amount = "amount",
-}
