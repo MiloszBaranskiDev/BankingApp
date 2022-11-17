405c5b43e65f2151163cc37f3ed832064eb536dc
Milosz <miloszbaranski@outlook.com>
2022-11-17 23:57:11 +0100

    add wrapper

--- src/components/ApiError.tsx
+++ src/components/ApiError.tsx
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 
+import StyledWrapper from "./styled/StyledWrapper";
+
 const ApiError: React.FC = (): JSX.Element => {
   return (
-    <StyledApiError>
-      An error occurred while loading data from API. Please try refreshing the
-      page.
-    </StyledApiError>
+    <StyledWrapper>
+      <StyledApiError>
+        An error occurred while loading data from API. Please try refreshing the
+        page.
+      </StyledApiError>
+    </StyledWrapper>
   );
 };
 
 export default ApiError;
 
 const StyledApiError = styled.p`
+  padding: 40px 0;
+  text-align: center;
   flex-basis: 100%;
   color: ${(props) => props.theme.colors.red};
 `;
