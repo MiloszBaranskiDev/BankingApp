19f1a3722e9b881cbb9d64200154d61341fa9086
Milosz <miloszbaranski@outlook.com>
2022-11-17 23:59:48 +0100

    rebuild for easier usage

--- src/redux/slices/TransactionsSlice.ts
+++ src/redux/slices/TransactionsSlice.ts
@@ -1,90 +1,68 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import { ECurrenciesSymbols } from "enums/ECurrenciesSymbols";
+import { ETransationType } from "enums/ETransactionType";
+import { ETransferKey } from "enums/ETransferKey";
+import { ECurrencySymbol } from "enums/ECurrencySymbol";
 import { ITransaction } from "interfaces/ITransaction";
-import { TransactionCategory } from "enums/TransactionCategory";
+import { ETransactionCategory } from "enums/ETransactionCategory";
 
 const initialState: ITransaction[] = [
   {
-    category: TransactionCategory.outgoing,
+    type: ETransationType.outcome,
+    category: ETransactionCategory.outgoingTransfer,
+    currencySymbol: ECurrencySymbol.pln,
+    amount: 1200,
     date: "2022-06-11",
     id: "6r6deb4d-6b6d-4bad-9bdd-1b0d7b3dcb6d",
-    details: [
-      {
-        label: "Recipient",
-        value: "John Smith",
-      },
-      {
-        label: "Account No.",
-        value: "123456789987654321",
-      },
-      {
-        label: "Address",
-        value: "Lorem ipsum 16/4",
-      },
-      {
-        label: "Currency",
-        value: ECurrenciesSymbols.pln,
-      },
-      {
-        label: "Amount",
-        value: 400,
-      },
-      {
-        label: "Title",
-        value: "Car repair",
-      },
-    ],
+    details: {
+      [ETransferKey.title]: "Car repair",
+      [ETransferKey.recipient]: "John Smith",
+      [ETransferKey.account]: "412456789987654321",
+      [ETransferKey.address]: "Lorem ipsum 11/2",
+      [ETransferKey.currencySymbol]: ECurrencySymbol.pln,
+      [ETransferKey.amount]: 1200,
+    },
   },
   {
-    category: TransactionCategory.incoming,
+    type: ETransationType.income,
+    category: ETransactionCategory.incomingTransfer,
+    currencySymbol: ECurrencySymbol.pln,
+    amount: 3000,
     date: "2022-06-12",
     id: "2r6aeb1d-6b6d-4bad-9bdd-1b0d7b3dcb6d",
-    details: [
-      {
-        label: "Sender",
-        value: "John Bloggs",
-      },
-      {
-        label: "Account No.",
-        value: "123456789987654321",
-      },
-      {
-        label: "Address",
-        value: "Lorem ipsum 16/4",
-      },
-      {
-        label: "Currency",
-        value: ECurrenciesSymbols.pln,
-      },
-      {
-        label: "Amount",
-        value: 1250,
-      },
-      {
-        label: "Title",
-        value: "Rent",
-      },
-    ],
+    details: {
+      [ETransferKey.title]: "Rent",
+      [ETransferKey.sender]: "John Bloggs",
+      [ETransferKey.account]: "123456789987654321",
+      [ETransferKey.address]: "Lorem ipsum 16/4",
+      [ETransferKey.currencySymbol]: ECurrencySymbol.pln,
+      [ETransferKey.amount]: 3000,
+    },
   },
   {
-    category: TransactionCategory.swap,
+    type: ETransationType.swap,
+    category: ETransactionCategory.swap,
+    currencySymbol: null,
+    amount: null,
     date: "2022-06-13",
     id: "3r3deb3d-6b6d-4bad-9bdd-1b0d7b3dcb6d",
-    details: [
-      {
-        label: "From",
-        value: `88 ${ECurrenciesSymbols.usd.toUpperCase()}`,
-      },
-      {
-        label: "To",
-        value: `396 ${ECurrenciesSymbols.pln.toUpperCase()}`,
-      },
-      {
-        label: "Exchange rate",
-        value: "4.5",
-      },
-    ],
+    details: {
+      from: `88 ${ECurrencySymbol.usd.toUpperCase()}`,
+      to: `396 ${ECurrencySymbol.pln.toUpperCase()}`,
+      rate: 4.5,
+    },
+  },
+  {
+    type: ETransationType.income,
+    category: ETransactionCategory.goalCancel,
+    currencySymbol: ECurrencySymbol.eur,
+    amount: 400,
+    date: "2022-06-19",
+    id: "1r7adb7d-2b4d-1dad-4bca-1b0d7b3dcb6d",
+    details: {
+      title: "Phone",
+      returned: `400 ${ECurrencySymbol.eur}`,
+    },
   },
 ];
 
@@ -93,8 +71,17 @@ export const TransactionsSlice = createSlice({
   initialState,
   reducers: {
     addTransaction: (state, action: PayloadAction<ITransaction>) => {
-      const { category, date, id, details } = action.payload;
-      state.push({ category: category, date: date, id: id, details: details });
+      const { type, category, currencySymbol, amount, date, id, details } =
+        action.payload;
+      state.push({
+        type: type,
+        category: category,
+        currencySymbol: currencySymbol,
+        amount: amount,
+        date: date,
+        id: id,
+        details: details,
+      });
     },
   },
 });
