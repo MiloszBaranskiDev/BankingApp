ebc81af9061c65c747c6d8ea76845ffc495782c6
Milosz <miloszbaranski@outlook.com>
2022-12-19 00:14:01 +0100

    fix styles, fix api error when date is lower than expected

--- src/components/pages/Currencies/parts/Chart.tsx
+++ src/components/pages/Currencies/parts/Chart.tsx
@@ -4,13 +4,14 @@ import styled, { useTheme } from "styled-components";
 import GetTodayDate from "utils/GetTodayDate";
 import GetCurrencyHistoricalPrices from "api/GetCurrencyHistoricalPrices";
 
+import { ITheme } from "interfaces/ITheme";
 import { ILineChartData } from "interfaces/ILineChartData";
 import { ECurrencySymbol } from "enums/ECurrencySymbol";
 
 import ApiError from "components/ApiError";
 import StyledTile from "components/styled/StyledTile";
 import LineChart from "components/LineChart";
-import Loader from "components/Loader";
+import LoaderSmall from "components/LoaderSmall";
 import StyledHeading from "components/styled/StyledHeading";
 import ChartDates from "../elements/ChartDates";
 
@@ -18,50 +19,54 @@ interface IProps {
   currencySymbol: Exclude<ECurrencySymbol, ECurrencySymbol.pln>;
 }
 
-enum ECalculateActions {
-  add = "add",
-  subtract = "subract",
+enum ECalculateAction {
+  add,
+  subtract,
 }
 
-const calculateDate = (
-  comparedDate: string,
-  action: ECalculateActions,
-  diff: number
-) => {
-  let date: Date | string = new Date(comparedDate);
+const apiMinDate: Date = new Date("2002-01-03");
 
-  if (action === ECalculateActions.add) {
+const changeDateToString = (date: Date): string => {
+  return date.toISOString().split("T")[0];
+};
+
+const calculateDate = (
+  comparedDate: Date,
+  action: ECalculateAction,
+  diff: number
+): Date => {
+  let date: Date = new Date(comparedDate);
+
+  if (action === ECalculateAction.add) {
     date.setDate(date.getDate() + diff);
-  } else if (action === ECalculateActions.subtract) {
+  } else if (action === ECalculateAction.subtract) {
     date.setDate(date.getDate() - diff);
   }
-
-  date = date.toISOString().split("T")[0];
 
   return date;
 };
 
 const Chart: React.FC<IProps> = ({ currencySymbol }) => {
-  const theme: any = useTheme();
+  const theme = useTheme() as ITheme;
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [showError, setShowError] = useState<boolean>(false);
   const [chartData, setChartData] = useState<ILineChartData>(null as any);
 
-  const [endDate, setEndDate] = useState<string>(GetTodayDate());
-  const [startDate, setStartDate] = useState<string>(
-    calculateDate(endDate, ECalculateActions.subtract, 180)
+  const [endDate, setEndDate] = useState<Date>(new Date(GetTodayDate()));
+  const [startDate, setStartDate] = useState<Date>(
+    calculateDate(endDate, ECalculateAction.subtract, 180)
   );
 
-  const [minStartDate, setMinStartDate] = useState<string>();
-  const [minEndDate, setMinEndDate] = useState<string>();
-  const [maxStartDate, setMaxStartDate] = useState<string>();
+  const [minStartDate, setMinStartDate] = useState<Date>();
+  const [minEndDate, setMinEndDate] = useState<Date>();
+  const [maxStartDate, setMaxStartDate] = useState<Date>();
 
-  const loadPrices = async (start: string, end: string) => {
+  const loadPrices = async (start: Date, end: Date) => {
     const loadedPrices = await GetCurrencyHistoricalPrices(
       currencySymbol,
-      start,
-      end
+      changeDateToString(start),
+      changeDateToString(end)
     );
 
     if (loadedPrices === undefined) {
@@ -84,9 +89,9 @@ const Chart: React.FC<IProps> = ({ currencySymbol }) => {
 
   useEffect(() => {
     loadPrices(startDate, endDate);
-    setMinStartDate(calculateDate(endDate, ECalculateActions.subtract, 354));
-    setMinEndDate(calculateDate(startDate, ECalculateActions.add, 14));
-    setMaxStartDate(calculateDate(endDate, ECalculateActions.subtract, 14));
+    setMinStartDate(calculateDate(endDate, ECalculateAction.subtract, 354));
+    setMinEndDate(calculateDate(startDate, ECalculateAction.add, 14));
+    setMaxStartDate(calculateDate(endDate, ECalculateAction.subtract, 14));
   }, [startDate, endDate]);
 
   useEffect(() => {
@@ -97,6 +102,10 @@ const Chart: React.FC<IProps> = ({ currencySymbol }) => {
         : setIsLoading(true);
     }
   }, [chartData]);
+
+  useEffect(() => {
+    startDate! < apiMinDate && setStartDate(apiMinDate);
+  }, [startDate]);
 
   return (
     <StyledTile as={StyledChart}>
@@ -110,7 +119,7 @@ const Chart: React.FC<IProps> = ({ currencySymbol }) => {
         setStartDate={setStartDate}
         setEndDate={setEndDate}
       />
-      {isLoading && <Loader />}
+      {isLoading && <LoaderSmall />}
       {!isLoading && showError ? <ApiError /> : null}
       {!isLoading && !showError ? <LineChart chartData={chartData} /> : null}
     </StyledTile>
@@ -125,19 +134,14 @@ const StyledChart = styled.div`
     text-transform: uppercase;
   }
   canvas {
-    margin-top: 30px;
+    flex-basis: 100%;
   }
-
   @media (min-width: ${(props) => props.theme.breakpoints.tablet}) {
     display: flex;
     flex-wrap: wrap;
     h2 {
       margin-right: auto;
       padding-right: 35px;
-    }
-    canvas {
-      margin-top: 0;
-      flex-basis: 100%;
     }
   }
 `;
